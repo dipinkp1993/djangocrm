f32e5336abce9e9a00be0935c58e21cfbc75da7d
Dipin KP <dipinpnambiar@gmail.com>
2020-06-25 16:53:26 +0530

    register with customized form1

--- accounts/views.py
+++ accounts/views.py
@@ -8,9 +8,9 @@ from django.core.paginator import Paginator
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
-	form = UserCreationForm
+	form = CreateUserForm
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = CreateUserForm(request.POST)
 		if form.is_valid():
 			form.save()
 			return redirect('/')
