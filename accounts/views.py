112ede927cb8f30f61da00f334bebc0080140842
Dipin KP <dipinpnambiar@gmail.com>
2020-06-26 15:22:57 +0530

    user profile settings  of customer with pic created

--- accounts/views.py
+++ accounts/views.py
@@ -82,6 +82,19 @@ def userPage(request):
 			   'delivered': delivered, 'pending': pending}
 	return render(request, 'accounts/users.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+	customer = request.user.customer
+	form = CustomerForm(instance=customer)
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST, request.FILES,instance=customer)
+		if form.is_valid():
+			form.save()
+	context = {'form': form}
+	return render(request, 'accounts/account_settings.html', context)
+
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
