ebffb4ff7d1f6b8692dc17402be4dffc36e2c32c
Dipin KP <dipinpnambiar@gmail.com>
2020-06-26 18:33:45 +0530

    heroko deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn crm1:wsg1 --log-file -
--- crm1/settings.py
+++ crm1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'zpvhvmc@&tx5+fgs+%j(yzc^-+^=t_br0tgqdo9692!#$2px1&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dipinworks.herokuapp.com','127.0.0.1']
 
 
 # Application definition
